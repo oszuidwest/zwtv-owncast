606cd04219a008de73ce6d94855c9dab9447d229
rmens <r.mens@me.com>
2023-01-17 23:04:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # BIG WORK IN PROGRESS
 
-apt install ffmpeg nginx -y
+apt install ffmpeg nginx certbot -y
 mkdir -p /var/lib/owncast/database
-useradd owncast --system
+useradd owncast --system --shell /bin/false
